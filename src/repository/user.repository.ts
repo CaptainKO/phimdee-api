e0c329e22a4259ca113c2986e32902ebab353851
captainKO <17521098@gm.uit.edu.vn>
2020-01-13 16:31:24 +0700

    add user

--- src/repository/user.repository.ts
+++ src/repository/user.repository.ts
@@ -2,12 +2,11 @@ import {
   EntityRepository, Repository, getRepository 
 } from "typeorm";
 import { User } from "@entity/user.entity";
-import * as util from 'util';
 export type GenderType = "male" | "female";
 export enum Gender {
   male = "male", female = "female"
 }
 @EntityRepository(User)
-export class StudentRepository extends Repository<User> {
+export class UserRepository extends Repository<User> {
 
 }
