13b5d10b7165f2befd2d35effa33d162cfdff1c3
captainKO <17521098@gm.uit.edu.vn>
2020-01-14 22:02:15 +0700

    change import

--- src/validators/IsUserAlreadyExist.ts
+++ src/validators/IsUserAlreadyExist.ts
@@ -1,12 +1,11 @@
 import { registerDecorator, ValidationOptions, ValidatorConstraint, ValidatorConstraintInterface, ValidationArguments } from "class-validator";
-import { UserRepository } from "src/repository/user.repository";
+import userRepository from "src/repository/user.repository";
 
 @ValidatorConstraint({ async: true })
 export class IsUserAlreadyExistConstraint implements ValidatorConstraintInterface {
 
   validate(userName: any, args: ValidationArguments) {
-    // @ts-ignore
-    return UserRepository.findOneByName(userName).then(user => {
+    return userRepository.findOne(userName).then(user => {
       if (user) return false;
       return true;
     });
