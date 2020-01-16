e0bd461fe3e48b3f52e3b3cc09bd53b5674f04b3
captainKO <17521098@gm.uit.edu.vn>
2020-01-16 14:14:47 +0700

    use local database

--- src/repository/user.repository.ts
+++ src/repository/user.repository.ts
@@ -1,14 +1,13 @@
-import { 
+import {
   EntityRepository, Repository,
 } from "typeorm";
 import { User } from "@entity/user.entity";
-export type GenderType = "male" | "female";
-export enum Gender {
-  male = "male", female = "female"
-}
+
 @EntityRepository(User)
 class UserRepository extends Repository<User> {
+  // create() {
 
+  // }
 }
 
 export default new UserRepository();
